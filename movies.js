e185148016c8e899818d9f44482df8b62b16b6b4
Azhar6512 <2023515617.mohammad@ug.sharda.ac.in>
2025-11-27 10:33:40 +0530

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,7 @@
+let movies = [
+  { id: 1, title: "Inception", rating: 9 },
+  { id: 2, title: "Interstellar", rating: 8.5 },
+  { id: 3, title: "Dangal", rating: 8.4 },
+];
+
+module.exports = movies;
